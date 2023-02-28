6a7da0aa2e6dea697a0659397cb4f2af5c393617
cyanhat <72936856+cyanhat@users.noreply.github.com>
2023-02-28 10:45:26 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
 echo "INSTALL PYTHON AND PIP"
+
 pip install pyfiglet
+
 git clone https://github.com/tasos-py/Search-Engines-Scraper
+
+echo "IF THIS FAILS PLEASE 'CD' INTO SEARCH-ENGINES-SCRAPER'S DIRECTORY"
+
+python3 setup.py install
